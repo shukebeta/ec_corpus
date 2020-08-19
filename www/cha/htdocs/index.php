21eccc71a322a62c27a9c2ea66796f483ad29270
shukebeta <weizhong2004@gmail.com>
2020-08-19 14:20:28 +1200

    fix style issue

--- www/cha/htdocs/index.php
+++ www/cha/htdocs/index.php
@@ -9,12 +9,12 @@ echo <<<html
     <meta charset="utf-8" />
     <meta name="renderer" content="webkit" />  <!-- 360浏览器访问页面默认用极速核 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> <!-- 优先使用IE最新版本 -->
-    <meta name="description" content="查例句, 你输入一个词汇，或者一个短语，我帮你找出例句。" />
+    <meta name="description" content="查例句: 输入一个词汇，或者一个短语，得到中英文例句。" />
     <meta name="keywords" content="查例句" />
     <title>例句：{$keyword}</title>
     <style>
 	body {
-          margin: 20px 20%;
+          margin: 20px 5%;
         }
         #query {
 	  position: fixed;
@@ -25,12 +25,13 @@ echo <<<html
 <body>
 <form id="queryform" action="/">
 <div id="query">
-<input id="inputbox" type="text" name="keyword" value="$keyword_input" style="font-size:18px; width:400px;height:50px" />
-<button type="submit" style="font-size:18px;width:100px;height:50px">查例句</button> <br />
+<input id="inputbox" type="text" name="keyword" value="$keyword_input" style="font-size:1.5em; width:400px;height:50px" />
+<button type="submit" style="font-size:1.2em;width:4.5em;height:50px"> 查例句</button>
+<br />
 </div>
 <div style="margin-top:5em;">
-<p style="font-size:12px; ">Tips：</p>
-<ul style="font-size:12px">
+<p style="font-size:1em;">Tips：</p>
+<ul style="font-size:1em;">
 <li>想只匹配 <code>world</code> 而不匹配 <code>worldcup</code>之类的词, 输入 <code>world\b</code></li>
 <li>想只匹配 <code>worldcup</code> 之类的词而不匹配纯粹的 <code>world</code>, 输入 <code>world\B</code></li>
 <li>如果你很懂正则表达式，它还能更强大</li>
@@ -46,7 +47,7 @@ input.onpaste = function(e) {
 };
 </script>
 
-<h2>匹配结果</h2>
+<h2>例句</h2>
 html;
 
 $dict = [
@@ -58,7 +59,7 @@ if (strpos($keyword, '+') && strpos($keyword, '\\+') == false) {
 }
 
 $keyword = strtr($keyword, $dict);
-$result = `grep -B1 -A1 -ihr -e "${keyword}" ../data`;
+$result = `grep -B2 -A1 -ihr -e "${keyword}" ../data`;
 
 if ($result) {
     // to avoid very big html page, limit the search result to less then 100K

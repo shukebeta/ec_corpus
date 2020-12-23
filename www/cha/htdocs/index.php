f22b6fa744958aa23bf50eb2650af43b26e22def
shukebeta <weizhong2004@gmail.com>
2020-12-24 11:42:53 +1300

    small improvement when keyword is in chinese with no result

--- www/cha/htdocs/index.php
+++ www/cha/htdocs/index.php
@@ -75,9 +75,9 @@ if ($result) {
     $result = '抱歉，关键字<font color="red"> ' . $keyword_input . ' </font>一个例句也没找到。';
 }
 $k = str_replace('\b', '', $keyword);
-$k = urlencode(preg_replace("/[^a-z ]/i", "", $k));
+$k = urlencode($k);
 $result .= "<p>其他词典 <a href='https://www.dictionary.com/browse/{$k}?s=t' target='_blank'>Dictionary</a>";
-$result .= " | <a href='http://dict.youdao.com/w/{$k}' target='_blank'>有道词典</a>";
+$result .= " | <a href='http://dict.youdao.com/w/eng/{$k}' target='_blank'>有道词典</a>";
 $result .= " | <a href='https://dictionary.cambridge.org/dictionary/english/{$k}' target='_blank'>Cambridge Dictionary</a>";
 $result .= " | <a href='https://vocabulary.com/dictionary/{$k}' target='_blank'>Vocabulary.com</a>";
 
